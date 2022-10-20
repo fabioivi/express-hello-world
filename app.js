6d94196d8a0ad6429976b3df259f92c8e6a0b09a
Fábio Duarte <fabio.ivi@gmail.com>
2022-10-20 13:49:03 -0400

    feat: add support to post

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.get('/req', (req, res) => {
