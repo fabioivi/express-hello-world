a2ac145ab9c4a9c1b019c99e3ffdff4dad11563f
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-11-24 20:57:02 -0400

    desaf2

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-
+// desafio 1
 app.get("/meunome", (req, res) => {
   res.send("Meu nome é Fábio Barreto de Oliveira");
+});
+
+// desafio 2
+app.get("/eco", (req, res) => {
+  res.send("eco");
 });
